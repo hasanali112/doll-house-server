843211c53afb92ed2af2ea6f749500df585fb81f
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-05-21 03:36:47 +0600

    add a toy using post method and store data in mongodb

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_username)
+
 
 const uri = `mongodb+srv://${process.env.DB_username}:${process.env.DB_password}@cluster1.4gey7ap.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
@@ -29,7 +29,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const userReviewCollection = client.db('reviewDb').collection('review')
+    const createToyCollection = client.db('addAtoyDB').collection('addtoy')
 
+    //for add a toy
+    app.post('/addtoys', async (req, res)=>{
+        const addAtoy = req.body;
+        console.log(addAtoy);
+        const result = await createToyCollection.insertOne(addAtoy)
+        res.send(result);
+    })
+
+    //receive data from client
+    app.post('/reviews', async (req, res)=>{
+        const reviewCollection = req.body;
+        console.log(reviewCollection)
+        const result = await userReviewCollection.insertOne(reviewCollection)
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
