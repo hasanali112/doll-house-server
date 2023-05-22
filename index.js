80c978bba2f571fe101e65363c1b550842ac320f
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-05-23 02:51:51 +0600

    final update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
+    // client.connect();
 
     const userReviewCollection = client.db("reviewDb").collection("review");
     const createToyCollection = client.db("addAtoyDB").collection("addtoy");
